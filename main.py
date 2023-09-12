14b5bf7f6d4c363d1613fc0b27c70427dc69fea9
juanmanuel-moratilla-sonarsource <juanmanuel.moratilla@sonarsource.com>
2023-09-12 13:19:54 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+import mysql.connector
+
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+    c = 0
+    b = 1
+    if c == 0:
+        if c == 0:
+            print("c == 0")
+
+    connection = mysql.connector.connect(host='localhost',
+                                         database='mydb',
+                                         user='nouser',
+                                         password='')
+
+
+
+    connection.close()
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
